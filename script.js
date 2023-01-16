bfed31ef4fd9392c3be82c3760b5d506b96cf9f5
Jo Wu <jo.wu@student.unsw.edu.au>
2023-01-17 07:20:56 +1100

    added games section

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ pop.addEventListener('mouseup', () => {
 // no drag
 document.getElementById('imgClickAndChange').ondragstart = function () { return false; };
 document.getElementById('gif').ondragstart = function () { return false; };
+
+// scroll reveal
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
